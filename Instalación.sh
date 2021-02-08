cfad34e8505bf0caa2244b4fa46e1fc3997017fc
root <root@debian10.example.net>
2021-02-08 16:25:10 +0100

    Con caché

--- Instalación.sh
+++ Instalación.sh
@@ -5,4 +5,5 @@ sudo apt-get install squid
 sudo cp squid.conf /etc/squid/squid.conf
 sudo cp urls-bloqueadas.txt /etc/squid/urls-bloqueadas.txt 
 sudo cp archivos_a_bloquear.txt /etc/squid/archivos_a_bloquear.txt 
+sudo cp sysctl.conf /etc/sysctl.conf
 sudo systemctl restart squid 
