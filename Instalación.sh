5053c00a24b5eb4e2ff483a888eef3e417ef5bb8
root <root@debian10.example.net>
2021-02-11 17:08:31 +0100

    Con ldap

--- Instalación.sh
+++ Instalación.sh
@@ -6,4 +6,6 @@ sudo cp squid.conf /etc/squid/squid.conf
 sudo cp urls-bloqueadas.txt /etc/squid/urls-bloqueadas.txt 
 sudo cp archivos_a_bloquear.txt /etc/squid/archivos_a_bloquear.txt 
 sudo cp sysctl.conf /etc/sysctl.conf
+sudo cp hosts /etc/hosts
+sudo cp hostname /etc/hostname
 sudo systemctl restart squid 
